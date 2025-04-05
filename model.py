09d8a33cc13554af129b3f83b91076c8d6fe41ff
thanmay <thanmaym0804@gmail.com>
2025-04-06 02:56:06 +0530

    Initial commit: Live face emotion detection using EfficientNet B4

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
+
+class EmotionEfficientNet(nn.Module):
+    def __init__(self, num_classes):
+        super(EmotionEfficientNet, self).__init__()
+        self.model = EfficientNet.from_pretrained("efficientnet-b4")
+        in_features = self.model._fc.in_features
+        self.model._fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
